36e48adba28bdaf9adf5f9dbe69e6312fd11c2f2
Simon Edelmann <admin@simonedelmann.de>
2020-02-21 13:47:43 +0100

    Better abstraction

--- Sources/CrudAPI/CrudController.swift
+++ Sources/CrudAPI/CrudController.swift
@@ -1,14 +1,42 @@
 import Vapor
 import Fluent
 
-public struct CrudController<T: Crudable & Model & Content> where T.IDValue: LosslessStringConvertible {
-    func indexAll(req: Request) -> EventLoopFuture<[T]> {
-        return T.query(on: req.db).all()
+protocol CrudControllerProtocol {
+    associatedtype ModelT: Crudable & Model & Content where ModelT.IDValue: LosslessStringConvertible
+}
+
+extension CrudControllerProtocol {
+    internal static func indexAll(on database: Database) -> EventLoopFuture<[ModelT]> {
+        return ModelT.query(on: database).all()
     }
     
-    func index(req: Request) -> EventLoopFuture<T> {
-        let id: T.IDValue? = req.parameters.get("id")
-        return T.find(id, on: req.db)
-            .unwrap(or: Abort(.notFound))
+    internal static func index(_ id: ModelT.IDValue?, on database: Database) -> EventLoopFuture<ModelT> {
+        return ModelT.find(id, on: database).unwrap(or: Abort(.notFound))
     }
 }
+
+public struct CrudController<T: Crudable & Model & Content>: CrudControllerProtocol where T.IDValue: LosslessStringConvertible {
+    typealias ModelT = T
+    
+    static func indexAll(req: Request) -> EventLoopFuture<[T]> {
+        indexAll(on: req.db)
+    }
+    
+    static func index(req: Request) -> EventLoopFuture<T> {
+        let id: T.IDValue? = req.parameters.get("id")
+        return index(id, on: req.db)
+    }
+}
+
+extension CrudController where T: Publicable {
+    static func indexAll(req: Request) -> EventLoopFuture<[T.Public]> {
+        indexAll(on: req.db).public()
+    }
+
+    static func index(req: Request) -> EventLoopFuture<T.Public> {
+        let id: T.IDValue? = req.parameters.get("id")
+        return index(id, on: req.db).public()
+    }
+}
+
+
