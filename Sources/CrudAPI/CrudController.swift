98404b0a43a5c576a1c4da7d1052c8b7d90e5ff5
Simon Edelmann <admin@simonedelmann.de>
2020-02-23 21:55:52 +0100

    Require Publicable protocol

--- Sources/CrudAPI/CrudController.swift
+++ Sources/CrudAPI/CrudController.swift
@@ -1,28 +1,16 @@
 import Vapor
 import Fluent
 
-public struct CrudController<T: Model & Content>: CrudControllerProtocol where T.IDValue: LosslessStringConvertible {
+struct CrudController<T: Model & Content & Publicable>: CrudControllerProtocol where T.IDValue: LosslessStringConvertible {
     typealias ModelT = T
     
-    func indexAll(req: Request) -> EventLoopFuture<[T]> {
-        indexAll(on: req.db)
-    }
-    
-    func index(req: Request) -> EventLoopFuture<T> {
-        let id: T.IDValue? = req.parameters.get("id")
-        return index(id, on: req.db)
-    }
-}
-
-extension CrudController where T: Publicable {
     func indexAll(req: Request) -> EventLoopFuture<[T.Public]> {
         indexAll(on: req.db).public()
     }
-
+    
     func index(req: Request) -> EventLoopFuture<T.Public> {
         let id: T.IDValue? = req.parameters.get("id")
         return index(id, on: req.db).public()
     }
 }
-
 
--- Sources/CrudAPI/Routes.swift
+++ Sources/CrudAPI/Routes.swift
@@ -2,12 +2,6 @@ import Vapor
 import Fluent
 
 extension RoutesBuilder {
-    public func crud<T: Model & Content>(model: T.Type) where T.IDValue: LosslessStringConvertible {
-        let crudController = CrudController<T>()
-        self.get(use: crudController.indexAll)
-        self.get(":id", use: crudController.index)
-    }
-
     public func crud<T: Model & Content & Publicable>(model: T.Type) where T.IDValue: LosslessStringConvertible {
         let crudController = CrudController<T>()
         self.get(use: crudController.indexAll)
