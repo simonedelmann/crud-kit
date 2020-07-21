8f1ee661670bc783d536f75fa271f6ca48744b82
Simon Edelmann <admin@simonedelmann.de>
2020-07-21 11:17:50 +0200

    Fix deprecation warning

--- Sources/CRUDKit/Extensions/Content+Validatable.swift
+++ Sources/CRUDKit/Extensions/Content+Validatable.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Content {
     internal static func validate(on request: Request) throws {
         if let validatable = Self.self as? Validatable.Type {
-            try validatable.validate(request)
+            try validatable.validate(content: request)
         }
     }
 }
